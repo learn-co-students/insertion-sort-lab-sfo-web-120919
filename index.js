700dace161d7eaea7f0595a36672e3d287d6e012
coronaisme <coronaisme@gmail.com>
2020-07-10 11:58:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+  let currentMin = array[0];
+  let minIndex = 0;
 
+  array.forEach((el, i) => {
+    if (el < currentMin) {
+      currentMin = el;
+      minIndex = i;
+    }
+  });
+
+  array.splice(minIndex, 1);
+  return currentMin;
 }
 
-function insertionSort(array){
-
-}
+function insertionSort(array) {}
